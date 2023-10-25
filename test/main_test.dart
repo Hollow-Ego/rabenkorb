d10afb5b3344290841467042399d062bc9ae9cf7
Alexander Pahn <alexander.pahn@outlook.com>
2023-10-25 12:55:39 +0200

    Setup CI Workflow

---
+++ test/main_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:rabenkorb/main.dart';
+
+
+void main() {
+  testWidgets('MainApp displays "Hello World!"', (WidgetTester tester) async {
+    await tester.pumpWidget(const MainApp());
+    expect(find.text('Hello World!'), findsOneWidget);
+  });
+}
