470db51ded7993b06891f6bbbe430688c8c370ce
Alexander Pahn <alexander.pahn@outlook.com>
2024-04-14 15:46:40 +0200

    improved debugging page and added log debug widget

--- lib/features/core/debug/debug_page.dart
+++ lib/features/core/debug/debug_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rabenkorb/features/core/debug/language_debug.dart';
+import 'package:rabenkorb/features/core/debug/logging_debug.dart';
 import 'package:rabenkorb/features/core/structural/core_scaffold.dart';
 import 'package:rabenkorb/features/core/structural/drawer/core_drawer.dart';
 import 'package:rabenkorb/generated/l10n.dart';
@@ -10,11 +11,13 @@ class DebugPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CoreScaffold(
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: GridView.count(
+          crossAxisCount: 2,
+          children: const [
             LanguageDebug(),
+            LoggingDebug(),
           ],
         ),
       ),
--- lib/features/core/debug/language_debug.dart
+++ lib/features/core/debug/language_debug.dart
@@ -11,25 +11,27 @@ class LanguageDebug extends StatelessWidget with WatchItMixin {
   @override
   Widget build(BuildContext context) {
     final _ = watchStream((IntlStateService p0) => p0.locale);
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(S.of(context).LanguageDisplayName),
-          Text(S.of(context).EmptyMessage),
-          CorePrimaryButton(
-            onPressed: () {
-              di<IntlStateService>().setLocale(const Locale('de', 'DE'));
-            },
-            child: const Text("Deutsch"),
-          ),
-          CoreSecondaryButton(
-            onPressed: () {
-              di<IntlStateService>().setLocale(const Locale('en'));
-            },
-            child: const Text("English"),
-          )
-        ],
+    return Card(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(S.of(context).LanguageDisplayName),
+            Text(S.of(context).EmptyMessage),
+            CorePrimaryButton(
+              onPressed: () {
+                di<IntlStateService>().setLocale(const Locale('de', 'DE'));
+              },
+              child: const Text("Deutsch"),
+            ),
+            CoreSecondaryButton(
+              onPressed: () {
+                di<IntlStateService>().setLocale(const Locale('en'));
+              },
+              child: const Text("English"),
+            )
+          ],
+        ),
       ),
     );
   }
