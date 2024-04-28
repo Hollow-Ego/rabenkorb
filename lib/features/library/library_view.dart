db4880ee4ba8df9ba74538650c9ae4cfe3bce025
Alexander Pahn <alexander.pahn@outlook.com>
2024-04-28 16:16:41 +0200

    library view can display grouped items

--- lib/features/library/library_view.dart
+++ lib/features/library/library_view.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:rabenkorb/features/core/display/core_grouped_list.dart';
+import 'package:rabenkorb/models/grouped_items.dart';
+import 'package:rabenkorb/models/item_template_view_model.dart';
+import 'package:rabenkorb/services/business/library_service.dart';
+import 'package:watch_it/watch_it.dart';
 
-class LibraryView extends StatelessWidget {
+class LibraryView extends StatelessWidget with WatchItMixin {
   const LibraryView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      key: Key('library:placeholder'),
-    );
+    final AsyncSnapshot<List<GroupedItems<ItemTemplateViewModel>>> templates = watchStream((LibraryService p0) => p0.itemTemplates, initialValue: []);
+
+    return CoreGroupedList(
+        items: templates.data,
+        itemContentBuilder: (BuildContext context, ItemTemplateViewModel item) {
+          return Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(item.name),
+            ),
+          );
+        });
   }
 }
