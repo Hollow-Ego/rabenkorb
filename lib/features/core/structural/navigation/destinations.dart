25c9218e1a53b39e1ed58b5d6c6cdab68322595b
Alexander Pahn <alexander.pahn@outlook.com>
2024-04-28 13:20:32 +0200

    initialized library view

--- lib/features/core/structural/navigation/destinations.dart
+++ lib/features/core/structural/navigation/destinations.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rabenkorb/features/core/structural/navigation/destination_details.dart';
+import 'package:rabenkorb/features/library/library_main_action.dart';
+import 'package:rabenkorb/features/library/library_view.dart';
 import 'package:rabenkorb/generated/l10n.dart';
 
 final List<DestinationDetails> mainDestinations = [
@@ -16,16 +18,11 @@ final List<DestinationDetails> mainDestinations = [
   ),
   DestinationDetails(
     destination: NavigationDestination(
-      icon: const Icon(Icons.bug_report),
-      label: S.current.Debug,
+      icon: const Icon(Icons.library_books),
+      label: S.current.Library,
     ),
-    body: const Center(
-      child: Text("Debug 2"),
-    ),
+    body: const LibraryView(),
     index: 1,
-    mainAction: MainAction(
-      icon: const Icon(Icons.access_alarm),
-      onPressed: (BuildContext context) {},
-    ),
+    mainAction: libraryMainAction,
   )
 ];
---
+++ lib/features/library/library_main_action.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:rabenkorb/features/core/structural/navigation/destination_details.dart';
+
+final libraryMainAction = MainAction(
+  onPressed: (BuildContext context) {},
+);
