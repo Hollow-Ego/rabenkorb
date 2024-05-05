e51ccad55b22d8484932d712a2b7b02ef0158b38
Alexander Pahn <alexander.pahn@outlook.com>
2024-05-05 12:37:34 +0200

    added test to ensure deleting of item template works

--- integration_test/helper/test_helper.dart
+++ integration_test/helper/test_helper.dart
@@ -2,13 +2,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:rabenkorb/abstracts/preference_service.dart';
 import 'package:rabenkorb/database/database.dart';
 import 'package:rabenkorb/di/di_setup.dart';
 import 'package:rabenkorb/features/core/debug/debug_database_helper.dart';
 import 'package:rabenkorb/services/state/basket_state_service.dart';
 import 'package:rabenkorb/services/state/intl_state_service.dart';
 import 'package:rabenkorb/services/state/library_state_service.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:watch_it/watch_it.dart';
 
 const pumpDuration = Duration(milliseconds: 500);
@@ -19,9 +19,8 @@ void testErrorHandler(FlutterErrorDetails details) {
 }
 
 Future<void> resetAppState() async {
-  final prefs = await SharedPreferences.getInstance();
-  await prefs.clear();
-  di<IntlStateService>().init();
+  await di<PreferenceService>().clear();
+  await di<IntlStateService>().init();
   di<LibraryStateService>().init();
   di<BasketStateService>().init();
 }
