57459aa7ab12a2e50f0419af32bd639f528d5f77
Alexander Pahn <alexander.pahn@outlook.com>
2024-05-05 10:57:58 +0200

    navigation bar updates the selected index again

--- lib/features/core/structural/navigation/core_navigation.dart
+++ lib/features/core/structural/navigation/core_navigation.dart
@@ -3,7 +3,9 @@ import 'package:rabenkorb/services/state/navigation_state_service.dart';
 import 'package:watch_it/watch_it.dart';
 
 class CoreNavigation extends StatelessWidget {
-  const CoreNavigation({super.key});
+  final int pageIndex;
+
+  const CoreNavigation({super.key, required this.pageIndex});
 
   @override
   Widget build(BuildContext context) {
--- lib/models/main_page_details.dart
+++ lib/models/main_page_details.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:rabenkorb/features/core/structural/navigation/destination_details.dart';
 
 class MainPageDetails {
+  final int pageIndex;
   final Widget? body;
   final MainAction? mainAction;
   final PreferredSizeWidget? appBar;
 
-  MainPageDetails({this.body, this.mainAction, this.appBar});
+  MainPageDetails({required this.pageIndex, this.body, this.mainAction, this.appBar});
 }
