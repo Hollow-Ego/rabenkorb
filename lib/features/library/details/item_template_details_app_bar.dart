be9b17b21f451e98cfc761fc4bf60c0ebab09e8a
Alexander Pahn <alexander.pahn@outlook.com>
2024-05-15 17:42:50 +0200

    item template details app bar shows new or edit depending on mode

--- lib/features/library/details/item_template_details_app_bar.dart
+++ lib/features/library/details/item_template_details_app_bar.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:rabenkorb/generated/l10n.dart';
 
 class ItemTemplateDetailsTitle extends StatelessWidget {
-  const ItemTemplateDetailsTitle({super.key});
+  final bool isNewItem;
+
+  const ItemTemplateDetailsTitle({super.key, required this.isNewItem});
 
   @override
   Widget build(BuildContext context) {
-    return Text("");
+    return Text(isNewItem ? S.of(context).New : S.of(context).Edit);
   }
 }
