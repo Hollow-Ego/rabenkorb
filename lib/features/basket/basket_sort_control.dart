945e4d00835a63b786203842506a3586485a6300
Alexander Pahn <alexander.pahn@outlook.com>
2024-05-16 07:34:21 +0200

    refactored sort rule selector into searchable dropdown

--- lib/features/basket/basket_sort_control.dart
+++ lib/features/basket/basket_sort_control.dart
@@ -5,7 +5,7 @@ import 'package:rabenkorb/services/business/basket_service.dart';
 import 'package:rabenkorb/services/state/basket_state_service.dart';
 import 'package:rabenkorb/shared/default_sort_rules.dart';
 import 'package:rabenkorb/shared/sort_mode.dart';
-import 'package:rabenkorb/shared/widgets/inputs/core_dropdown_button.dart';
+import 'package:rabenkorb/shared/widgets/form/core_searchable_dropdown.dart';
 import 'package:rabenkorb/shared/widgets/inputs/core_icon_text_button.dart';
 import 'package:watch_it/watch_it.dart';
 
@@ -22,60 +22,70 @@ class BasketSortControl extends StatelessWidget with WatchItMixin {
     final sortMode = watchStream((BasketStateService p0) => p0.sortMode, initialValue: basketStateService.sortModeSync);
     final sortDirection = watchStream((BasketStateService p0) => p0.sortDirection, initialValue: basketStateService.sortDirectionSync);
     final selectedItem = sortOrder.hasData && (availableSortRules.data?.length ?? 0) > 0
-        ? sortOrder.data!
-        : (sortMode.data == SortMode.databaseOrder ? sortByDatabasePseudoId : sortByNamePseudoId);
+        ? availableSortRules.data!.firstWhere((e) => e.id == sortOrder.data!, orElse: () => _getDefaultSelectedItem(sortMode, sortRules))
+        : _getDefaultSelectedItem(sortMode, sortRules);
     sortRules.addAll(availableSortRules.data ?? []);
 
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        CoreDropdownButton<int>(
-          icon: const Icon(
-            Icons.sort_by_alpha,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Expanded(
+            child: CoreSearchableDropdown<SortRuleViewModel>(
+              selectedItem: selectedItem,
+              items: sortRules,
+              itemAsString: (SortRuleViewModel? rule) => _getSortRuleName(context, rule),
+              displayString: (SortRuleViewModel? rule) => _getSortRuleName(context, rule),
+              onChanged: _updateSortDetails,
+              compareFn: (a, b) => a.id == b.id,
+              inputDecoration: const InputDecoration(
+                border: InputBorder.none,
+              ),
+              allowEmptyValue: false,
+            ),
           ),
-          onPressed: _updateSortDetails,
-          selectedItem: selectedItem,
-          items: _toDropdownMenuItem(sortRules),
-        ),
-        CoreIconTextButton(
-          icon: const Icon(Icons.sort),
-          label: Text(S.of(context).SortDirection(sortDirection.data?.name ?? "")),
-          onPressed: () async {
-            await basketStateService.switchSortDirection();
-          },
-        ),
-      ],
+          CoreIconTextButton(
+            icon: const Icon(Icons.sort),
+            label: Text(S.of(context).SortDirection(sortDirection.data?.name ?? "")),
+            onPressed: () async {
+              await basketStateService.switchSortDirection();
+            },
+          ),
+        ],
+      ),
     );
   }
 
-  Future<void> _updateSortDetails(int? id) async {
-    if (id == null) {
+  String _getSortRuleName(BuildContext context, SortRuleViewModel? rule) {
+    return rule?.name ?? S.of(context).Unnamed;
+  }
+
+  SortRuleViewModel _getDefaultSelectedItem(AsyncSnapshot<SortMode> sortMode, List<SortRuleViewModel> sortRules) {
+    if (sortMode.data == SortMode.databaseOrder) {
+      return sortRules.firstWhere((e) => e.id == sortByDatabasePseudoId);
+    }
+    return sortRules.firstWhere((e) => e.id == sortByNamePseudoId);
+  }
+
+  Future<void> _updateSortDetails(SortRuleViewModel? rule) async {
+    final ruleId = rule?.id;
+    if (ruleId == null) {
       return;
     }
     final basketStateService = di<BasketStateService>();
-    if (id == sortByNamePseudoId) {
+    if (ruleId == sortByNamePseudoId) {
       await basketStateService.setSortMode(SortMode.name);
       await basketStateService.setSortRuleId(null);
       return;
     }
-    if (id == sortByDatabasePseudoId) {
+    if (ruleId == sortByDatabasePseudoId) {
       await basketStateService.setSortMode(SortMode.databaseOrder);
       await basketStateService.setSortRuleId(null);
       return;
     }
 
     await basketStateService.setSortMode(SortMode.custom);
-    await basketStateService.setSortRuleId(id);
-  }
-
-  List<DropdownMenuItem<int>> _toDropdownMenuItem(List<SortRuleViewModel> sortRules) {
-    return sortRules
-        .map(
-          (sortRule) => DropdownMenuItem<int>(
-            value: sortRule.id,
-            child: Text(sortRule.name),
-          ),
-        )
-        .toList();
+    await basketStateService.setSortRuleId(ruleId);
   }
 }
