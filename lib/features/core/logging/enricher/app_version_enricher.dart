1da84d26c32d11e5467e1916329a2eee0745d781
Alexander Pahn <alexander.pahn@outlook.com>
2024-05-19 13:30:24 +0200

    expanded app version enricher

--- lib/features/core/logging/enricher/app_version_enricher.dart
+++ lib/features/core/logging/enricher/app_version_enricher.dart
@@ -1,10 +1,13 @@
+import 'package:rabenkorb/services/core/environment_service.dart';
 import 'package:rabenkorb/services/core/version_service.dart';
 import 'package:watch_it/watch_it.dart';
 
 void enrichWithAppVersion(Map<String, dynamic> data) {
   final versionService = di<VersionService>();
+  final envService = di<EnvironmentService>();
   final internalVersion = versionService.internalAppVersion;
   final publicVersion = versionService.publicAppVersion;
   data["internal_app_version"] = internalVersion;
   data["public_app_version"] = publicVersion;
+  data["environment"] = envService.environment;
 }
