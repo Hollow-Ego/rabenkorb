e8900cb047cbf282a6f5cb159f65c0e5e9826604
Alexander Pahn <alexander.pahn@outlook.com>
2024-05-20 12:03:07 +0200

    added background image

--- lib/features/core/structural/core_scaffold.dart
+++ lib/features/core/structural/core_scaffold.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rabenkorb/features/core/display/loading_overlay.dart';
+import 'package:rabenkorb/shared/widgets/display/background_image.dart';
 
 class CoreScaffold extends StatelessWidget {
   final String? titleText;
@@ -28,18 +29,21 @@ class CoreScaffold extends StatelessWidget {
     return LoadingOverlay(
       child: GestureDetector(
         onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-        child: Scaffold(
-          key: scaffoldKey,
-          // backgroundColor: Colors.transparent,
-          appBar: appBar,
-          body: body != null
-              ? SafeArea(
-                  child: body!,
-                )
-              : null,
-          drawer: drawer,
-          bottomNavigationBar: bottomNavigationBar,
-          floatingActionButton: floatingActionButton,
+        child: BackgroundImage(
+          image: "assets/images/background.png",
+          child: Scaffold(
+            key: scaffoldKey,
+            backgroundColor: Colors.transparent,
+            appBar: appBar,
+            body: body != null
+                ? SafeArea(
+                    child: body!,
+                  )
+                : null,
+            drawer: drawer,
+            bottomNavigationBar: bottomNavigationBar,
+            floatingActionButton: floatingActionButton,
+          ),
         ),
       ),
     );
