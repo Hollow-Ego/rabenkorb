3bd0c6035ad1dcd26e33cd084b48c8acb482aa9c
Alexander Pahn <alexander.pahn@outlook.com>
2024-05-26 11:59:18 +0200

    in shopping mode fab allows for removing checked items

--- lib/models/main_page_details.dart
+++ lib/models/main_page_details.dart
@@ -4,9 +4,10 @@ import 'package:rabenkorb/shared/destination_details.dart';
 class NavigationPageDetails {
   final int pageIndex;
   final Widget? body;
+  final Widget? fab;
   final MainAction? mainAction;
   final PreferredSizeWidget? appBar;
   final bool hideFabInShoppingMode;
 
-  NavigationPageDetails({required this.pageIndex, this.body, this.mainAction, this.appBar, this.hideFabInShoppingMode = false});
+  NavigationPageDetails({required this.pageIndex, this.body, this.mainAction, this.appBar, this.hideFabInShoppingMode = false, this.fab});
 }
